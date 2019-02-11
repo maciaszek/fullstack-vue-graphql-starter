ea361ac030adff9551e553e7eb08f585ff878fed
Maciej Drazek <maciej.drazek@o2.pl>
2019-02-11 23:21:49 +0100

    section 4 - complete

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const { ApolloServer, gql } = require("apollo-server");
 const mongoose = require("mongoose");
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8');
+const resolvers = require('./resolvers.gql');
+
 require("dotenv").config({ path: "variables.env" });
 
+const User = require('./models/user');
+const Post = require('./models/post');
+
+mongoose.Promise = global.Promise;
+mongoose.set('debug', true);
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {useNewUrlParser: true})
   .then(() => console.log("DB connected"))
   .catch(err => console.error(err));
 
@@ -12,19 +24,13 @@ const todos = [
   { task: "Clean room", completed: true }
 ];
 
-const typeDefs = gql`
-  type Query {
-    getTodos: [Todo]
-  }
-
-  type Todo {
-    task: String
-    completed: Boolean
-  }
-`;
-
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  resolvers,
+  context: {
+    User,
+    Post
+  }
 });
 
 server.listen().then(({ url }) => {
